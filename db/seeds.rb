5e0e9da89ed140f7d2fa0ade5036e1b169e89028
Sander Voerman <sander@pixelgilde.nl>
2015-12-06 16:35:49 +0100

    Create the devise user, configure devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create( name: 'Admin', email: 'admin@admin.com', password: 'admin123', password_confirmation: 'admin123' )
